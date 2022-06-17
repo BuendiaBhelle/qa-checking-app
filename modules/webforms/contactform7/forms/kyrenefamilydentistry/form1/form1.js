e5546941ea609f1b469b29e6e81a28d340a6c585
BuendiaBhelle <BuendiaBhelle@gmail.com>
2022-06-17 09:29:18 -0700

    UPDATES: fixed kfd webforms

--- modules/webforms/contactform7/forms/kyrenefamilydentistry/form1/form1.js
+++ modules/webforms/contactform7/forms/kyrenefamilydentistry/form1/form1.js
@@ -24,7 +24,7 @@ async function webforms(domain, timestamp, forms, range_thankyou_page, module_na
         await driver.findElement(By.name("tel-101")).sendKeys("4806480839");
         await driver.findElement(By.name("textarea-101")).sendKeys("Please take note that this is a test submit form for Quick Contact. Please disregard if received. Thank you.");
         await driver.executeScript("return document.getElementsByName('acceptance-101')[0].click()");
-        await driver.executeScript("return document.getElementsByClassName('btn-primary')[4].click()");
+        await driver.executeScript("return document.getElementsByClassName('wpcf7-form-control has-spinner wpcf7-submit btn btn-primary d-block w-100')[0].click()");
         logger.logger.log({ level: 'info', message: 'WEBFORMS - form fill in success.', tester: server.userId });
         console.log("WEBFORMS - form fill in success.");
         value = [ "", "", "info", "form fill in success.", server.userId, timestamp, module_name, domain, "", "", "", launch, "", webforms, "", "" ];
